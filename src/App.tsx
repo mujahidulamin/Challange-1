aae13403a1d4627abc23c3ed91c801855603444a
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2024-01-20 03:25:49 +0600

    actions dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 
-import { useAppDispatch, useAppSelector } from "./redux/hooks";
-import { useSelector } from "react-redux";
+
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "./redux/store";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
 
 function App() {
 
 const {count} = useSelector((state: RootState) => state.counter)
 
 
+const dispatch = useDispatch();
+
 
   return (
     <div>
       <div className="flex gap-6">
-        <button className="border-2 rounded-md px-2 py-3 border-green-500 ">
+        <button className="border-2 rounded-md px-2 py-3 border-green-500 " onClick={() => dispatch(increment())}>
           Increment
         </button>
         <div>{count}</div>
-        <button className="border-2 rounded-md px-2 py-3 border-red-500 ">
+        <button className="border-2 rounded-md px-2 py-3 border-red-500 " onClick={() => dispatch(decrement())}>
           Decrement
         </button>
       </div>
