59c0afa0043ccac49a9a0c3f4f1d56494a252928
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2024-01-20 03:18:35 +0600

    create counter slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
+
 function App() {
+
+const {count} = useSelector((state: RootState) => state.counter)
+
+
+
   return (
-    <h1 className="bg-red-500">Tech Net</h1>
-  )
+    <div>
+      <div className="flex gap-6">
+        <button className="border-2 rounded-md px-2 py-3 border-green-500 ">
+          Increment
+        </button>
+        <div>{count}</div>
+        <button className="border-2 rounded-md px-2 py-3 border-red-500 ">
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
